6a0266e4f0a7687c7ea2e9c58f26f16d679cb9c9
aycakman <aycaakman0@gmail.com>
2023-05-22 16:44:15 +0300

    used Authentication model to register

--- ChatApp/Controllers/RegisterViewController.swift
+++ ChatApp/Controllers/RegisterViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var passwordTextField: UITextField!
@@ -17,7 +19,16 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func registerButtonPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    print(error)
+                }else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
 
+            }
+        }
         
     }
     
