db298cc02217dda4d32dc25a5e64b5e63524dc4b
aycakman <aycaakman0@gmail.com>
2023-05-22 13:23:42 +0300

    added animation for initial VC

--- ChatApp/Controllers/WelcomeViewController.swift
+++ ChatApp/Controllers/WelcomeViewController.swift
@@ -13,18 +13,19 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "Welcome Chat App"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
