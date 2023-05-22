90f08e571477de5d973883872e39643ed492b10e
aycakman <aycaakman0@gmail.com>
2023-05-22 15:21:04 +0300

    changed animation using CLTypingLabel

--- ChatApp/Controllers/WelcomeViewController.swift
+++ ChatApp/Controllers/WelcomeViewController.swift
@@ -6,22 +6,23 @@
 //
 
 import UIKit
-
+import CLTypingLabel
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "Welcome Chat App"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "Welcome Chat App"
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "Welcome Chat App"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
         // Do any additional setup after loading the view.
     }
     
