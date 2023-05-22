07d8524e4f065f38865f014eede79165475bf1f4
aycakman <aycaakman0@gmail.com>
2023-05-22 17:04:33 +0300

    used Authentication model to sign in

--- ChatApp/Controllers/LoginViewController.swift
+++ ChatApp/Controllers/LoginViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-
+import FirebaseCore
+import FirebaseAuth
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var passwordTextField: UITextField!
@@ -19,15 +20,18 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                }else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+             
+            }
+        }
+       
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
